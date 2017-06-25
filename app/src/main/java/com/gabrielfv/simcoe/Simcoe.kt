6e33e0d75c06e6ed4820d72e9c7a40c9fd0cf497
Gabriel Vasconcelos <gabriel@gabrielfv.com>
2017-06-25 00:31:59 -0300

    Flushing whole project, preparing for Kodein

--- app/src/main/java/com/gabrielfv/simcoe/Simcoe.kt
+++ app/src/main/java/com/gabrielfv/simcoe/Simcoe.kt
@@ -1,23 +1,13 @@
 package com.gabrielfv.simcoe
 
 import android.app.Application
-import com.gabrielfv.simcoe.di.AppComponent
-import com.gabrielfv.simcoe.di.AppModule
+import android.arch.persistence.room.Room
+import com.github.salomonbrys.kodein.*
 
 /**
  * @author gabriel on 6/15/17.
  */
-class Simcoe : Application() {
-
-    val component: AppComponent by lazy {
-        DaggerAppComponent
-                .builder()
-                .appModule(AppModule(this))
-                .build()
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        component.inject(this)
+class Simcoe : Application(), KodeinAware {
+    override val kodein: Kodein by Kodein.lazy {
     }
 }
